626b9a7b4012e59efaf3b25d54bcbfdb8d6dfced
dodi177 <zsigmond.dorian01@gmail.com>
2025-09-10 08:45:31 +0200

    Add project files.

---
+++ vinnipu/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vinnipu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double elsoszam = 0;
+        private string muvelet = "";
+        private bool ujErtek=false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+		private void szam_Click(object sender, RoutedEventArgs e)
+		{
+            var gomb = (Button)sender;
+            var szam= gomb.Content.ToString();
+            if (kijelzo.Text=="0" || ujErtek)
+            {
+                kijelzo.Text = szam;
+                ujErtek = false;
+            }
+            else
+            {
+                kijelzo.Text += szam;
+            }
+		}
+	}
+}
